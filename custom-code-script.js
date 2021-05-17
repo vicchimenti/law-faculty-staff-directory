5dc62a6e4946c05aa27f2fe1c30be5352156638e
Vic Chimenti <victor.chimenti@gmail.com>
2021-05-17 11:43:38 -0700

    import custom script

---
+++ custom-code-script.js
@@ -0,0 +1,51 @@
+<script>
+function getAllUrlParams(url) {
+  var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+  var obj = {};
+  if (queryString) {
+    queryString = queryString.split('#')[0];
+    var arr = queryString.split('&');
+
+    for (var i = 0; i < arr.length; i++) {
+      var a = arr[i].split('=');
+      var paramName = a[0];
+      var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+
+      // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+      if (paramName.match(/\[(\d+)?\]$/)) {
+        var key = paramName.replace(/\[(\d+)?\]/, '');
+        if (!obj[key]) obj[key] = [];
+        // if it's an indexed array e.g. colors[2]
+        if (paramName.match(/\[\d+\]$/)) {
+          var index = /\[(\d+)\]/.exec(paramName)[1];
+          obj[key][index] = paramValue;
+        } else {
+          obj[key].push(paramValue);
+        }
+      } else {
+        if (!obj[paramName]) {
+          // if it doesn't exist, create property
+          obj[paramName] = paramValue;
+        } else if (obj[paramName] && typeof obj[paramName] === 'string'){
+          // if property does exist and it's a string, convert it to an array
+          obj[paramName] = [obj[paramName]];
+          obj[paramName].push(paramValue);
+        } else {
+          obj[paramName].push(paramValue);
+        }
+      }
+    }
+  }
+  return obj;
+}
+var searchBoxValue = "";
+var searchUrlEnc = getAllUrlParams().keyword;
+
+if (searchUrlEnc){
+   var searchUrl = decodeURIComponent(searchUrlEnc);
+   console.log("search URL" + searchUrl + "       searcboxvalue: " + searchBoxValue.length);
+  searchBoxValue = searchUrl.replace(/[&\/\\#,+()$~%.:*?]/g, ' ');
+}
+
+$("#keyword").val(searchBoxValue);
+</script>
