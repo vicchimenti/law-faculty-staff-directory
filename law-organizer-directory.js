f5e2291b13b1498affb0e84044ab543552f749ac
Vic Chimenti <victor.chimenti@gmail.com>
2021-05-17 14:02:50 -0700

    cm unused writes

--- law-organizer-directory.js
+++ law-organizer-directory.js
@@ -27,7 +27,7 @@
     var emailAddress = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Email' output='normal' modifiers='striptags,htmlentities,encode_emails' />");
     var firstName = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='FirstName' output='normal' modifiers='striptags,htmlentities' />");
     var lastName = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='LastName' output='normal' modifiers='striptags,htmlentities' />");
-    var primaryTitle = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Title' output='normal' modifiers='striptags,htmlentities />");
+    var title = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Title' output='normal' modifiers='striptags,htmlentities />");
     var phone = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Phone' output='normal' modifiers='striptags,htmlentities />");
     var streetAddress = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Street Address' output='normal' modifiers='striptags,htmlentities />");
     var htmlOutput = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='HTMLOutput' output='normal' modifiers='striptags,htmlentities />");
@@ -63,13 +63,13 @@
      var openHiddenFields = '<div class="hiddenSearchText visually-hidden">';
      var closeHiddenFields = '</div>';
      var emailAddressString = '<p class="card-link email"><a href="mailto:' + emailAddress + '?subject=From your Directory Profile" title="Email ' + firstName + ' ' + lastName + '">Contact ' + firstName + '</a></p>';
-     var primaryTitleString = '<p class="card-text title">' + primaryTitle + '</p>';
+     var titleString = '<p class="card-title title">' + title + '</p>';
      var departmentString = '<p class="card-text division">' + department + '</p>';
      var streetAddressString = '<p class="card-text address">' + streetAddress + '</p>';
      var phoneString = '<p class="card-text phone">' + phone + '</p>';
      var openCardBody = '<div class="card-body">';
      var closeCardBody = '</div>';
-     var cardTitle = '<h4 class="card-title">' + firstName + ' ' + lastName + '</h4>';
+     var cardHeader = '<h4 class="card-header">' + firstName + ' ' + lastName + '</h4>';
      var beginningHTML = '<div class="StaffListBox contentItem card w-100 border-0" aria-label="' + firstName + ' ' + lastName + '" id="id' + contentID + '" data-position-default="Main" data-position-selected="Main">';
      var endingHTML = '</div>';
      var horizontalRule = '<hr class="lawProfileBorderRule" />'
@@ -111,38 +111,38 @@
         var affiliationHidden = '<span class="visually-hidden affiliation">' + affiliation + '</span>';
         hiddenFields += affiliationHidden;
     }
-    if (education != "") {
-        var educationHidden = '<span class="visually-hidden education">' + education + '</span>';
-        hiddenFields += educationHidden;
-    }
-    if (expertise != "") {
-        var expertiseHidden = '<span class="visually-hidden expertise">' + expertise + '</span>';
-        hiddenFields += expertiseHidden;
-    }
-    if (affiliations != "") {
-        var affiliationsHidden = '<span class="visually-hidden affiliations">' + affiliations + '</span>';
-        hiddenFields += affiliationsHidden;
-    }
-    if (courses != "") {
-        var coursesHidden = '<span class="visually-hidden courses">' + courses + '</span>';
-        hiddenFields += coursesHidden;
-    }
-    if (biography != "") {
-        var biographyHidden = '<span class="visually-hidden biography">' + biography + '</span>';
-        hiddenFields += biographyHidden;
-    }
-    if (publications != "") {
-        var publicationsHidden = '<span class="visually-hidden publications">' + publications + '</span>';
-        hiddenFields += publicationsHidden;
-    }
-    if (activity != "") {
-        var activityHidden = '<span class="visually-hidden activity">' + activity + '</span>';
-        hiddenFields += activityHidden;
-    }
-    if (facultyStatus != "") {
-        var facultyStatusyHidden = '<span class="visually-hidden facultyStatus">' + facultyStatus + '</span>';
-        hiddenFields += facultyStatusyHidden;
-    }
+    // if (education != "") {
+    //     var educationHidden = '<span class="visually-hidden education">' + education + '</span>';
+    //     hiddenFields += educationHidden;
+    // }
+    // if (expertise != "") {
+    //     var expertiseHidden = '<span class="visually-hidden expertise">' + expertise + '</span>';
+    //     hiddenFields += expertiseHidden;
+    // }
+    // if (affiliations != "") {
+    //     var affiliationsHidden = '<span class="visually-hidden affiliations">' + affiliations + '</span>';
+    //     hiddenFields += affiliationsHidden;
+    // }
+    // if (courses != "") {
+    //     var coursesHidden = '<span class="visually-hidden courses">' + courses + '</span>';
+    //     hiddenFields += coursesHidden;
+    // }
+    // if (biography != "") {
+    //     var biographyHidden = '<span class="visually-hidden biography">' + biography + '</span>';
+    //     hiddenFields += biographyHidden;
+    // }
+    // if (publications != "") {
+    //     var publicationsHidden = '<span class="visually-hidden publications">' + publications + '</span>';
+    //     hiddenFields += publicationsHidden;
+    // }
+    // if (activity != "") {
+    //     var activityHidden = '<span class="visually-hidden activity">' + activity + '</span>';
+    //     hiddenFields += activityHidden;
+    // }
+    // if (facultyStatus != "") {
+    //     var facultyStatusyHidden = '<span class="visually-hidden facultyStatus">' + facultyStatus + '</span>';
+    //     hiddenFields += facultyStatusyHidden;
+    // }
 
 
 
@@ -153,23 +153,31 @@
      * */
     document.write(beginningHTML);
     document.write(anchorWrap);
-    document.write(openRow);
-    document.write(openImageWrapper);
-    document.write(primaryImageString);
-    document.write(closeImageWrapper);
-    document.write(openBodyWrapper);
+    document.write(cardHeader);
     document.write(openCardBody);
-    document.write(cardTitle);
-    document.write(primaryTitleString);
+    document.write(titleString);
+    document.write(departmentString);
+    document.write(streetAddressString);
+    document.write(phoneString);
     document.write(emailAddressString);
     document.write(closeCardBody);
-    document.write(closeBodyWrapper);
-    document.write(closeRow);
     document.write(openHiddenFields);
     document.write(hiddenFields);
     document.write(closeHiddenFields);
     document.write(horizontalRule);
     document.write(endingHTML);
+
+
+    // document.write(openRow);
+    // document.write(openImageWrapper);
+    // document.write(primaryImageString);
+    // document.write(closeImageWrapper);
+    // document.write(primaryTitleString);
+    // document.write(emailAddressString);
+    // document.write(closeBodyWrapper);
+    // document.write(closeRow);
+
+
 
 
 
